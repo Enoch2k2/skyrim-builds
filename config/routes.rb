34ee917490283b849ebae86c696e89b3f6d48d65
Enoch2k2 <enoch2k2@hotmail.com>
2019-10-08 12:54:16 -0500

    added jwt authorization

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  namespace :api do
+    resources :users
+  end
+  post '/auth/login', to: 'authentication#login'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
